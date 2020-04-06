efec7c7fd4d32555d2be06585c9c1a96e2baa7a4
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-04-06 17:26:38 +0200

    Creación component return y implementación del html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SearchComponent } from './components/search/search.component';
 import { MainComponent } from './pages/main/main.component';
+import { ReturnComponent } from './components/return/return.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SearchComponent,
-    MainComponent
+    MainComponent,
+    ReturnComponent
   ],
   imports: [
     BrowserModule,
