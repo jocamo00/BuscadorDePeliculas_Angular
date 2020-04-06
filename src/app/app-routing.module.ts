fa0fa0045ad49ae21a946a6249850bd2dc7c3faa
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-04-06 17:43:32 +0200

    Creación de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { FavoriteComponent } from './pages/favorite/favorite.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: MainComponent},
+  { path: 'favorite', component: FavoriteComponent},
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
