1aea504c818555046a02095b41757c87a83edde7
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-04-07 13:05:48 +0200

    Creada la función de cargar peliculas por nombre

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPeliculasService } from 'src/app/services/info-peliculas.service';
 
 @Component({
   selector: 'app-main',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(public infoPeliculasService: InfoPeliculasService ) { }
 
   ngOnInit(): void {
   }
--- src/app/services/info-peliculas.service.ts
+++ src/app/services/info-peliculas.service.ts
@@ -7,9 +7,11 @@ import { HttpClient } from '@angular/common/http';
 export class InfoPeliculasService {
 
   pelisFavorites: Array<any>;
+  pelisNombre: Array<any>;
 
   constructor( private http: HttpClient ) {
     this.cargarInfoPeliculasFavorites();
+    this.cargarInfoPeliculasNombre();
   }
 
 
@@ -20,4 +22,14 @@ export class InfoPeliculasService {
       console.log(resp.results);
     });
   }
+
+
+  private cargarInfoPeliculasNombre() {
+    this.http.get('https://api.themoviedb.org/3/search/movie?api_key=7f8494efa25bfa40d25abdd2fd435c69&language=en-US&query=spiderman&page=1&include_adult=false')
+      .subscribe((resp: any) => {
+        this.pelisNombre = resp.results;
+        console.log(resp.results);
+      });
+  }
+
 }
